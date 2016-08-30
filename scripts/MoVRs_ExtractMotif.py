ed2c1fcc87a394da25769dc24356c8cbbe71376b
Xiangyu Yao <xianyao@indiana.edu>
2016-08-30 20:51:14 +0800

    I've added extract_by_evalue function to MoVRs_ExtractMotif.py

--- scripts/MoVRs_ExtractMotif.py
+++ scripts/MoVRs_ExtractMotif.py
@@ -1,4 +1,4 @@
-#this script reads file containing list of motif names and print put several files each containg motifs for one group in MEME format.
+#this script reads file containing list of motif names and print out several files each containg motifs for one group in MEME format.
 #!/usr/bin/env python
 import argparse,math,re
 #ParseMEME parse the combined motif file into a dictionary, key is the name of motif, and value is a string containg all the content for this motif.
@@ -35,17 +35,49 @@ def check_threshold(motif,threshold):
 	if math.log(E_value/threshold)<=0: 
 		return True
 
+def extract_by_evalue(motif_list,evalue,k):
+	'''Take a dictionary of motif, e.g motif_list,evalue which default 
+	is 1e-10 and the maximum number k, then output corresponding motif'''
+	tmp_list={}
+	out_list=[]
+	for key in motif_list.keys():
+		motif=motif_list[key]
+		lines=motif.split('\n')
+		second=lines[1].strip()
+		E_value=second.split('E=')[1].strip()
+		E_value=float(E_value)
+		tmp_list[key]=E_value
+	sorted_pair=sorted(tmp_list.items(),key=lambda x: x[1])#sort the list by	the E_value from smallest to largest
+	i=0
+	while i<len(sorted_pair):
+		if E_value ==0:
+			i+=1
+		if math.log(E_value/evalue)<=0:
+			i+=1
+		else:
+			break
+	if k>i+1:
+		for j in range(i):
+			out_list.append(sorted_pair[j][0])
+	else:
+		for j in range(k):
+			out_list.append(sorted_pair[j][0])
+	return out_list
+
+	#out_list contains the motif names  	
+	
+	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i','--motif_MEME',required=True)
 	parser.add_argument('-n','--motif_name')
 	parser.add_argument('-t','--motif_threshold')
 	parser.add_argument('-o','--out_file',required=True)
+	parser.add_argument('-k','--motif_number',default=25)
 	args = parser.parse_args()
 
 	motif_list = ParseMEME(args.motif_MEME)
-
-
+	
 	#extract motifs according to threshold
 	if args.motif_threshold:
 		threshold = float(args.motif_threshold)
